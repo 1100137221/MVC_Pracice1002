56b8715eb8dd9bc4e3fd026f2b4d1401ed2b1ae8
1100137221 <tom603172002@gmail.com>
2016-10-02 06:43:45 +0800

    Use EF to add product

---
+++ MVC5_Pracice1002/Controllers/EFController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5_Pracice1002.Models;
+
+namespace MVC5_Pracice1002.Controllers
+{
+    public class EFController : Controller
+    {
+        // GET: EF
+        public ActionResult Index()
+        {
+            var db = new FabricsEntities();
+
+            db.Product.Add(new Product() {
+                ProductName = "BMW",
+                Price = 5,
+                Stock = 1,
+                Active = true
+            });
+
+            db.SaveChanges();
+
+            return View(db.Product.ToList());
+        }
+    }
+}
