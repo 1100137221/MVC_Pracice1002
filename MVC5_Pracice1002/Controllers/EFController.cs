0a2c621bdab0766076edb9128c28156f6161a44e
1100137221 <tom603172002@gmail.com>
2016-10-02 18:10:10 +0800

    pratice ef to RemoveRange

--- MVC5_Pracice1002/Controllers/EFController.cs
+++ MVC5_Pracice1002/Controllers/EFController.cs
@@ -17,25 +17,31 @@ namespace MVC5_Pracice1002.Controllers
         {
             
 
-            db.Product.Add(new Product() {
-                ProductName = "BMW",
-                Price = 5,
-                Stock = 1,
-                Active = true
-            });
+            //db.Product.Add(new Product() {
+            //    ProductName = "BMW",
+            //    Price = 5,
+            //    Stock = 1,
+            //    Active = true
+            //});
+            
+            var data = db.Product.OrderByDescending(p => p.ProductId).Take(20);
 
-           
+            foreach (var item in data)
+            {
+                item.Price += 1;
+            }
+
             try
             {
                 db.SaveChanges();
             }
             catch (DbEntityValidationException ex)
             {
-                foreach(DbEntityValidationResult item in ex.EntityValidationErrors)
+                foreach (DbEntityValidationResult item in ex.EntityValidationErrors)
                 {
                     string entityName = item.Entry.Entity.GetType().Name;
-                    
-                    foreach(DbValidationError err in item.ValidationErrors)
+
+                    foreach (DbValidationError err in item.ValidationErrors)
                     {
                         throw new Exception(entityName + " 驗證失敗:" + err.ErrorMessage);
                     }
@@ -43,13 +49,6 @@ namespace MVC5_Pracice1002.Controllers
                 }
                 throw new Exception(" 驗證失敗:");
             }
-
-            var data = db.Product.OrderByDescending(p => p.ProductId).Take(5);
-            foreach(var item in data)
-            {
-                item.Price += 1;
-            }
-            db.SaveChanges();
 
             return View(data);
         }
@@ -63,6 +62,10 @@ namespace MVC5_Pracice1002.Controllers
         public ActionResult Delete(int id)
         {
             var product = db.Product.Find(id);
+
+            //先刪除明細(多筆)
+            db.OrderLine.RemoveRange(product.OrderLine);
+            //再刪除產品
             db.Product.Remove(product);
             db.SaveChanges();
 
