547b4d039b510fb667a2988c3ceace3f540203ab
1100137221 <tom603172002@gmail.com>
2016-10-02 22:54:44 +0800

    Pratice defferent type ActionResult

---
+++ MVC5_Pracice1002/Controllers/VRController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using MVC5_Pracice1002.Models;
+
+namespace MVC5_Pracice1002.Controllers
+{
+    public class VRController : Controller
+    {
+        // GET: VR
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return PartialView("Index");
+        }
+
+        public ActionResult ContentTest()
+        {
+            return Content("<script>alert('msg...')</script>", "application/javascript", Encoding.UTF8);
+        }
+
+        public ActionResult FileTest()
+        {
+            return File(Server.MapPath(""), "image/png", "GoGo.png");
+        }
+
+        public ActionResult JsonTest()
+        {
+            var db = new FabricsEntities();
+
+            db.Configuration.LazyLoadingEnabled = false;
+
+            return Json(db.Product.Take(5), JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+}
--- MVC5_Pracice1002/Global.asax.cs
+++ MVC5_Pracice1002/Global.asax.cs
@@ -18,6 +18,9 @@ namespace MVC5_Pracice1002
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //ViewEngines.Engines.Clear();
+            //ViewEngines.Engines.Add(new RazorViewEngine());
         }
     }
 }
